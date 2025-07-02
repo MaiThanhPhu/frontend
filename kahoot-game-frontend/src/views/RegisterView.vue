f5d81283be6b84b5d955759e49dd41110ac412cb
MaiThanhPhu <phumai2004@gmail.com>
2025-07-02 07:18:44 +0700

    Update RegisterView.vue

--- kahoot-game-frontend/src/views/RegisterView.vue
+++ kahoot-game-frontend/src/views/RegisterView.vue
@@ -45,7 +45,7 @@ const handleRegister = async () => {
     return;
   }
   try {
-    const response = await axios.post('`${API_BASE_URL}/api/auth/login`', {
+    const response = await axios.post('`${API_BASE_URL}/api/auth/register`', {
       username: newUsername.value,
       email: newEmail.value,
       password: newPassword.value
